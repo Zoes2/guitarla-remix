0157821d4f676930fbdd5b7b3a110d1e99e55048
Zoes2 <v.meneses20.20@gmail.com>
2023-11-09 22:45:19 -0300

    optimización de codigo y creación listado de index

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,50 @@
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarras } from '~/models/guitarras.server'
+import { getPosts } from '~/models/posts.server'
+import stylesGuitarras from '~/styles/guitarras.css'
+import ListadoGuitarras from '../components/listado-guitarras'
+export function meta() {
+    return (
+        [
+            { title: 'GuitarLA - Tienda de Guitarras' },
+            { description: 'GuitarLA - Nuestra colección de guitarras' },
+        ]
+    )
+}
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: stylesGuitarras
+        }
+    ]
+}
+
+export async function loader() {
+
+    const [guitarras, posts] = await Promise.all([
+        getGuitarras(),
+        getPosts()
+    ])
+    return { 
+        guitarras: guitarras.data, 
+        posts: posts.data 
+    }
+}
 
 function Index() {
+
+    const { guitarras, posts } = useLoaderData()
+
     return (
-        <div>Hola</div>
+        <>
+            <main className='contenedor'>
+                <ListadoGuitarras
+                    guitarras={guitarras}
+                />
+            </main>
+        </>
     )
 }
 
